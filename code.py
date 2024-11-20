ce1a7b15c9cb1e53f75c1808a54f9e7aef8105a4
reuben-13 <reuben.koo@go.winona.edu>
2024-11-20 12:44:15 -0600

    making the playground release voltage.

--- code.py
+++ code.py
@@ -2,19 +2,21 @@ from adafruit_circuitplayground import cp
 import time
 import math
 import random
-
+from analogio import AnalogOut
+from analogio import AnalogIn
+import board
 count = 0
-
+analog_in = AnalogIn(board.A1)
+analog_out = AnalogOut(board.A0)
 while True:
     g = 9.8
     cp.pixels.brightness = 0.05
     x, y, z = cp.acceleration
     a = math.sqrt(x * x + y * y + z * z) / g
-
     if count == 0:
         t = time.monotonic() - time.monotonic()
-
     if t > random.randint(10, 20):
+        analog_out.value = 65535
         cp.pixels[1] = ((255, 0, 0))
         cp.pixels[2] = ((255, 75, 0))
         cp.pixels[3] = ((255, 255, 0))
@@ -27,8 +29,7 @@ while True:
         cp.pixels[0] = ((255, 255, 0))
         print("boom")
         time.sleep(900000000000)
-
     t += 1
     count += 1
     print(t)
-    time.sleep(1)
+    time.sleep(.1)
