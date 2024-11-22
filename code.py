d5030b8f440d175b74c2126804907b67ff0d9c5c
reuben-13 <reuben.koo@go.winona.edu>
2024-11-22 11:56:51 -0600

    Update code.py

--- code.py
+++ code.py
@@ -5,7 +5,9 @@ import random
 from analogio import AnalogOut
 from analogio import AnalogIn
 import board
+
 count = 0
+volt = 0
 analog_in = AnalogIn(board.A1)
 analog_out = AnalogOut(board.A0)
 while True:
@@ -16,21 +18,26 @@ while True:
     if count == 0:
         t = time.monotonic() - time.monotonic()
     if t > random.randint(10, 20):
-        analog_out.value = 65535
-        cp.pixels[1] = ((255, 0, 0))
-        cp.pixels[2] = ((255, 75, 0))
-        cp.pixels[3] = ((255, 255, 0))
-        cp.pixels[4] = ((255, 0, 0))
-        cp.pixels[5] = ((255, 75, 0))
-        cp.pixels[6] = ((255, 255, 0))
-        cp.pixels[7] = ((255, 0, 0))
-        cp.pixels[8] = ((255, 75, 0))
-        cp.pixels[9] = ((255, 255, 0))
-        cp.pixels[0] = ((255, 255, 0))
+        cp.pixels[1] = (255, 0, 0)
+        cp.pixels[2] = (255, 75, 0)
+        cp.pixels[3] = (255, 255, 0)
+        cp.pixels[4] = (255, 0, 0)
+        cp.pixels[5] = (255, 75, 0)
+        cp.pixels[6] = (255, 255, 0)
+        cp.pixels[7] = (255, 0, 0)
+        cp.pixels[8] = (255, 75, 0)
+        cp.pixels[9] = (255, 255, 0)
+        cp.pixels[0] = (255, 255, 0)
+        volt += 1
         print("boom")
-        time.sleep(900000000000)
-        
+        time.sleep(0.1)
+    if volt == 1:
+        analog_out.value = 65535
+        time.sleep(0.5)
+        break
+    cp.pixels.fill((255, 75, 0))
     t += 1
     count += 1
     print(t)
     time.sleep(1)
+
